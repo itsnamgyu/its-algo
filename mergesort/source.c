dcc8803037b2d0a9097ffd41269afc2cbf9c7be6
Namgyu Ho <itsnamgyu@gmail.com>
2017-09-22 03:22:28 +0900

    Added basic submit interface

---
+++ mergesort/source.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int list[100000]; 
+
+void mergesort(int low, int high);
+
+void merge(int lowA, int highA, int lowB, int highB);
+
+int main(void) {
+
+
+	return 0;
+}
+
+void mergesort(int low, int high) {
+
+}
+
+void merge(int lowA, int highA, int lowB, int highB) {
+
+}
---
+++ submit.c
@@ -0,0 +1,5 @@
+#include "submit.h"
+
+void submit() {
+
+}
